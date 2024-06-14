a61be9412a96ba4d4397d0aec0643ec9f3061db4
Tonny-Kioko <cfakiokoanthony@gmail.com>
2024-06-14 18:50:23 +0300

    testing models to ensure TDD is successful

--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
 class EntryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "is valid with valid attributes" do
+    entry = Entry.now(carbohydrates: 30, proteins: 30, fats: 40, calories: 500, meal_type: "desert", fod_type: "black forest")
+    assert entry.save
+    
+  end
 end
