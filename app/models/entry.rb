6037072849242838f648a2a2c7f98b20cf89a171
Tonny-Kioko <cfakiokoanthony@gmail.com>
2024-06-14 18:29:47 +0300

    grouping entries on the index file that were created on the current date

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,7 @@
 class Entry < ApplicationRecord
+  validates :calories, :proteins, :carbohydrates, :fats, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :meal_type, :food_item, presence: true
+
   def day
     created_at.to_date
   end
